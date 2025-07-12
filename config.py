7bb13b77499703cfd5fa97e3fcaec47b3af34817
Mr_ J <javad.seyyedi20006@gmail.com>
2025-07-12 13:25:06 +0330

    Modify config.py
    set standard config values for password generation

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Password configs
 PASSWORD_CONFIGS = {
-    "use_digits": True,
-    "use_lower": False,
-    "use_upper": True,
-    "use_special": True,
-    "length": 12
+    'use_digits': True,
+    'use_lower': True,
+    'use_upper': True,
+    'use_special': True,
+    'length': 12
 }
